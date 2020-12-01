4da93400dea1f67e340330c8d8233e7262170f19
Michelle Zhang <mzhang32@gmail.com>
2020-12-01 09:53:14 -0800

    working on making requests in model

--- Moonberry/src/main/java/com/revature/model/Employee.java
+++ Moonberry/src/main/java/com/revature/model/Employee.java
@@ -1,12 +1,59 @@
 package com.revature.model;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class Employee {
-	private int employee_id;
+	private int employeeId;
 	private String firstname;
 	private String lastname;
-	private int department_id;
-	private float tuition_avail;
-	private int direct_super;
+	private int departmentId;
+	private float tuitionAvail;
+	private int directSuper;
 	private String username;
 	private String password;
+	private ArrayList
+	
+
+	public Employee(int employeeId, String firstname, String lastname, int departmentId, int directSuper,
+			String username, String password) {
+		super();
+		this.employeeId = employeeId;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.departmentId = departmentId;
+		this.directSuper = directSuper;
+		this.username = username;
+		this.password = password;
+		this.tuitionAvail = 1000;
+	}
+
+
+	/**
+	 * Makes a request.
+	 * @param eventType
+	 * @param requestDate
+	 * @param requestAmount
+	 * @param eventStartDate
+	 * @param eventEndDate
+	 * @param eventName
+	 * @param eventLocation
+	 * @param eventDescription
+	 * @param gradeFormat
+	 * @param workJust
+	 * @return true if the request is valid.
+	 */
+	public boolean makeRequest(Event_Type eventType, LocalDate requestDate, float requestAmount, LocalDate eventStartDate, 
+			LocalDate eventEndDate, String eventName, String eventLocation, String eventDescription, 
+			Grade_Format gradeFormat, String workJust) {
+		//TODO: Make a request object
+		long untilEventStarts = requestDate.until(eventEndDate, ChronoUnit.DAYS);
+		
+		
+		return false;
+	}
+	
+	
+	
+	
 }
