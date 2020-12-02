8f1be731b5911203602bf3805ecd8e5a660ca10f
unknown <nadjahrobinson@gmail.com>
2020-12-02 14:28:39 -0500

    Added table creation script to file directory

--- Moonberry/src/main/java/com/revature/dao/EmployeeDao.java
+++ Moonberry/src/main/java/com/revature/dao/EmployeeDao.java
@@ -2,5 +2,7 @@ package com.revature.dao;
 
 public interface EmployeeDao {
 	//TODO save files to database
-	//TODO submit details to database
+	//TODO submit details to databas
+	//TODO  Michelle: add any method declarations besides the setters/getters/tostring 
+	//to this file and then again the gull methods in the matching impl file
 }
