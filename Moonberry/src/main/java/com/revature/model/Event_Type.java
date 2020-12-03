711cc2bbb8e18101e1a4ea849e70a0df8071737b
Michelle Zhang <mzhang32@gmail.com>
2020-12-02 19:04:54 -0800

    added reimbursement readjustment and added reimbursment amount to the request

--- Moonberry/src/main/java/com/revature/model/Event_Type.java
+++ Moonberry/src/main/java/com/revature/model/Event_Type.java
@@ -1,10 +1,20 @@
 package com.revature.model;
 
 public enum Event_Type {
-	UNIVERSITY_COURSE,
-	SEMINAR,
-	CERT_PREP_COURSE,
-	CERT,
-	TECH_TRAINING,
-	OTHER
+	UNIVERSITY_COURSE 	(0.8),
+	SEMINAR				(0.6),
+	CERT_PREP_COURSE	(0.75),
+	CERT				(1.0),
+	TECH_TRAINING		(0.9),
+	OTHER				(0.3);
+	
+	private final double rmbsment_coverage;
+	
+	Event_Type(double coverage) {
+		this.rmbsment_coverage = coverage;
+	}
+	
+	public double getRmbsmentCoverage() {
+		return this.rmbsment_coverage;
+	}
 }
