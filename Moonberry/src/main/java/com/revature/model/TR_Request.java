9bbc8fc6c314f35eed221084f608ca270d383ab8
Michelle Zhang <mzhang32@gmail.com>
2020-12-03 18:10:14 -0800

    changed LocalDate to LocalDateTime and added requestArrivalDate

--- Moonberry/src/main/java/com/revature/model/TR_Request.java
+++ Moonberry/src/main/java/com/revature/model/TR_Request.java
@@ -1,24 +1,25 @@
 package com.revature.model;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class TR_Request {
 
 	private RS requestStatus;
 	private int requestId;
 	private int employeeId;
-	private LocalDate requestDate;
-	private LocalDate eventDate;
+	private LocalDateTime requestMadeDate;
 	private String eventName;
 	private String eventLocation;
 	private String eventDescription;
-	private LocalDate eventStartDate;
-	private LocalDate eventEndDate;
+	private LocalDateTime eventStartDate;
+	private LocalDateTime eventEndDate;
 	private double tuitionAmount;
 	private double rmbsmentAmount;
 	private Grade_Format gradeF;
 	private String workJust;
 	private boolean urgent;
+	private LocalDateTime requestArrivalDate;
 	
 	public TR_Request() {
 		super();
@@ -26,16 +27,16 @@ public class TR_Request {
 	}
 
 
-	public TR_Request(RS requestStatus, int requestId, int employeeId, LocalDate requestDate, 
+	public TR_Request(RS requestStatus, int requestId, int employeeId, LocalDateTime requestMadeDate, 
 			String eventName, String eventLocation, String eventDescription,
-			LocalDate eventStartDate, LocalDate eventEndDate, double tuitionAmount, Grade_Format gradeF,
-			String workJust, boolean urgent) {
+			LocalDateTime eventStartDate, LocalDateTime eventEndDate, double tuitionAmount, Grade_Format gradeF,
+			String workJust, boolean urgent, LocalDateTime requestArrivalDate) {
 
 		super();
 		this.requestStatus = requestStatus;
 		this.requestId = requestId;
 		this.employeeId = employeeId;
-		this.requestDate = requestDate;
+		this.requestMadeDate = requestMadeDate;
 		this.eventName = eventName;
 		this.eventLocation = eventLocation;
 		this.eventDescription = eventDescription;
@@ -46,6 +47,7 @@ public class TR_Request {
 		this.gradeF = gradeF;
 		this.workJust = workJust;
 		this.urgent = urgent;
+		this.requestArrivalDate = requestArrivalDate;
 	}
 
 	public RS getRequestStatus() {
@@ -73,20 +75,12 @@ public class TR_Request {
 		this.employeeId = employeeId;
 	}
 
-	public LocalDate getRequestDate() {
-		return requestDate;
+	public LocalDateTime getRequestMadeDate() {
+		return requestMadeDate;
 	}
 
-	public void setRequestDate(LocalDate requestDate) {
-		this.requestDate = requestDate;
-	}
-
-	public LocalDate getEventDate() {
-		return eventDate;
-	}
-
-	public void setEventDate(LocalDate eventDate) {
-		this.eventDate = eventDate;
+	public void setRequestDate(LocalDateTime requestMadeDate) {
+		this.requestMadeDate = requestMadeDate;
 	}
 
 	public String getEventName() {
@@ -113,19 +107,19 @@ public class TR_Request {
 		this.eventDescription = eventDescription;
 	}
 
-	public LocalDate getEventStartDate() {
+	public LocalDateTime getEventStartDate() {
 		return eventStartDate;
 	}
 
-	public void setEventStartDate(LocalDate eventStartDate) {
+	public void setEventStartDate(LocalDateTime eventStartDate) {
 		this.eventStartDate = eventStartDate;
 	}
 
-	public LocalDate getEventEndDate() {
+	public LocalDateTime getEventEndDate() {
 		return eventEndDate;
 	}
 
-	public void setEventEndDate(LocalDate eventEndDate) {
+	public void setEventEndDate(LocalDateTime eventEndDate) {
 		this.eventEndDate = eventEndDate;
 	}
 
@@ -174,12 +168,15 @@ public class TR_Request {
 	@Override
 	public String toString() {
 		return "TR_Request [requestStatus=" + requestStatus + ", requestId=" + requestId + ", employeeId=" + employeeId
-				+ ", requestDate=" + requestDate + ", eventDate=" + eventDate + ", eventName=" + eventName
-				+ ", eventLocation=" + eventLocation + ", eventDescription=" + eventDescription + ", eventStartDate="
-				+ eventStartDate + ", eventEndDate=" + eventEndDate + ", tuitionAmount=" + tuitionAmount
-				+ ", rmbsmentAmount=" + rmbsmentAmount + ", gradeF=" + gradeF + ", workJust=" + workJust + ", urgent="
-				+ urgent + "]";
+				+ ", requestMadeDate=" + requestMadeDate + ", eventName=" + eventName + ", eventLocation="
+				+ eventLocation + ", eventDescription=" + eventDescription + ", eventStartDate=" + eventStartDate
+				+ ", eventEndDate=" + eventEndDate + ", tuitionAmount=" + tuitionAmount + ", rmbsmentAmount="
+				+ rmbsmentAmount + ", gradeF=" + gradeF + ", workJust=" + workJust + ", urgent=" + urgent
+				+ ", requestArrivalDate=" + requestArrivalDate + "]";
 	}
+
+
+
 	
 	
 	
