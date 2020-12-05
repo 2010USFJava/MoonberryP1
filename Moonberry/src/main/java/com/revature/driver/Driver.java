9405260b56486957040a1747bf52017e8648bb25
Michelle Zhang <mzhang32@gmail.com>
2020-12-04 16:30:53 -0800

    fixing things in make request

--- Moonberry/src/main/java/com/revature/driver/Driver.java
+++ Moonberry/src/main/java/com/revature/driver/Driver.java
@@ -1,10 +1,13 @@
 package com.revature.driver;
 
+import com.revature.model.Grade_Format;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		
-		
+		Grade_Format gradeFormat = Grade_Format.LETTER;
+		System.out.println(gradeFormat.toString());
 
 	}
 
